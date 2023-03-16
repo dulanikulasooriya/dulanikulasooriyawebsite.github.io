85a80d51f9a5d85b9e2f05f96d5563d43db552e2
dulanikulasooriya <dulanikulasooriya99@gmail.com>
2023-03-16 13:57:38 +0530

    first commit

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css";
+import Sidebar from "../../img/sidebar.PNG";
+import Ecommerce from "../../img/ecommerce.PNG";
+import HOC from "../../img/hoc.PNG";
+import MusicApp from "../../img/musicapp.jpg";
+import { themeContext } from "../../Context";
+const Portfolio = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div className="portfolio" id="portfolio">
+      {/* heading */}
+      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span>Portfolio</span>
+
+      {/* slider */}
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        grabCursor={true}
+        className="portfolio-slider"
+      >
+        <SwiperSlide>
+          <img src={Sidebar} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Ecommerce} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MusicApp} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={HOC} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Portfolio;
